ca69d4e36e5fb16fbf0f7098ca8285525e0ec113
Benjamin Reese <bareese@slac.stanford.edu>
2018-08-23 11:36:01 -0700

    Add arguments to GUI script for sim/eth/etc.

--- software/scripts/gui.py
+++ software/scripts/gui.py
@@ -3,10 +3,45 @@ import RebHeater
 import pyrogue.gui
 import PyQt4.QtGui
 import sys
+import argparse
 
 rogue.Logging.setFilter('pyrogue.SrpV3', rogue.Logging.Debug)
+# Set the argument parser
+parser = argparse.ArgumentParser()
 
-with RebHeater.LsstRebHeaterCtrlRoot(sim=False) as root:
+parser.add_argument(
+    "--ip", 
+    type     = str,
+    required = False,
+    default = '192.168.1.10',
+    help     = "IP address",
+)  
+
+parser.add_argument(
+    "--hwEmu", 
+    required = False,
+    action = 'store_true',
+    help     = "hardware emulation (false=normal operation, true=emulation)",
+)
+
+parser.add_argument(
+    "--sim", 
+    required = False,
+    action = 'store_true',
+    help     = "hardware emulation (false=normal operation, true=emulation)",
+)  
+
+parser.add_argument(
+    "--pollEn", 
+    required = False,
+    action = 'store_true',
+    help     = "enable auto-polling",
+)
+
+args = parser.parse_args()
+print(args)
+
+with RebHeater.LsstRebHeaterCtrlRoot(**vars(args)) as root:
     # Create GUI
     appTop = PyQt4.QtGui.QApplication(sys.argv)
     guiTop = pyrogue.gui.GuiTop(group='GUI')
